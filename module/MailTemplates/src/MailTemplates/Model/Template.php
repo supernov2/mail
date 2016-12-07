2397b42296673d058b9bff51bf91fe842b983961
Juan Pablo <pablo@cruzf.net>
2016-12-07 16:28:07 +0100

    added templates doctrine

--- module/MailTemplates/src/MailTemplates/Model/Template.php
+++ module/MailTemplates/src/MailTemplates/Model/Template.php
@@ -1,10 +1,44 @@
 <?php
 namespace MailTemplates\Model;
 
+
+use BjyAuthorize\Provider\Role\ProviderInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use ZfcUser\Entity\UserInterface;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="templates")
+ */
 class Template implements MailTemplateInterface
 {
+  /**
+   * [$id description]
+   * @var int
+   * @ORM\id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
   protected $id;
+  /**
+   * [$nombre description]
+   * @var string
+   * @ORM\Column(type="string", length=255)
+   */
   protected $nombre;
+
+  /**
+   * [$partials description]
+   * @var \Doctrine\Common\Collections\Collection
+   * @ORM\ManyToMany(targetEntity="partial")
+   * @ORM\JoinTable(name="templates_partials",
+   *      joinColumns={@ORM\JoinColumn(name="id_t", referencedColumnName="id")},
+   *      inverseJoinColumns={@ORM\JoinColumn(name="id_p", referencedColumnName="id")}
+   *    )
+   */
+  protected $partials;
 
     /**
      * Get the value of Id
