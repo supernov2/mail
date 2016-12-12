dca141db70cb6d33796aff3a1f2cd04c7a41a845
ukadev <mail@ukadev.com>
2016-12-12 10:38:23 +0100

    changed table name.

--- module/MailPartials/src/MailPartials/Model/Partial.php
+++ module/MailPartials/src/MailPartials/Model/Partial.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="templates_partials")
+ * @ORM\Table(name="partials")
  */
 class Partial implements MailPartialInterface
 {
